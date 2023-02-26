ac32071cd2a7a5b220c7012124f9ebbaf41a35e2
RafoBunyatyan <rafobuniatyan@mail.ru>
2023-02-26 19:40:11 +0400

    new button

---
+++ client/frontend.js
@@ -0,0 +1,38 @@
+import Vue from 'https://cdn.jsdelivr.net/npm/vue@2.6.11/dist/vue.esm.browser.js'
+
+new Vue({
+	el: '#app',
+	data() {
+		return {
+			form: {
+				name: '',
+				value: ''
+			},
+			contacts: [
+				{ id: 1, name: 'Rafo', value: '+7-921-100-20-30', marked: false }
+			]
+		}
+	},
+	computed: {
+		canCreate() {
+			return this.form.value.frim() && this.form.name.frim()
+		}
+	},
+	methods: {
+		createContact() {
+			const { ...contact } = this.form
+
+			this.contacts.push({ ...contact, id: Date.now() })
+
+			this.form.name = this.form.value = ''
+		},
+		markContact(id) {
+			const contact = this.contacts.find(c => c.id === id)
+			contact.marked = true
+
+		},
+		removeContact(id) {
+
+		}
+	}
+})
